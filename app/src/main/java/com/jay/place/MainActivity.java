adea12d7aaae5e9ddc6300d7ba7eaf535c2526f5
jayliu1989 <jayliu1989@live.cn>
2019-12-05 22:21:37 +0800

    update

--- app/src/main/java/com/jay/place/MainActivity.java
+++ app/src/main/java/com/jay/place/MainActivity.java
@@ -1,33 +1,32 @@
 package com.jay.place;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.content.Intent;
+import android.location.Address;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.PlaceLikelihood;
-import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
-import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
-import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.gson.Gson;
+import com.schibstedspain.leku.LocationPickerActivity;
 
-import java.util.Collections;
-import java.util.List;
+import static com.schibstedspain.leku.LocationPickerActivityKt.ADDRESS;
+import static com.schibstedspain.leku.LocationPickerActivityKt.LATITUDE;
+import static com.schibstedspain.leku.LocationPickerActivityKt.LOCATION_ADDRESS;
+import static com.schibstedspain.leku.LocationPickerActivityKt.LONGITUDE;
+import static com.schibstedspain.leku.LocationPickerActivityKt.TIME_ZONE_DISPLAY_NAME;
+import static com.schibstedspain.leku.LocationPickerActivityKt.TIME_ZONE_ID;
+import static com.schibstedspain.leku.LocationPickerActivityKt.TRANSITION_BUNDLE;
+import static com.schibstedspain.leku.LocationPickerActivityKt.ZIPCODE;
 
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
-    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 101;
-    PlacesClient placesClient;
+    private static final int MAP_BUTTON_REQUEST_CODE = 101;
     TextView tvResult;
+    public static final String API_KEY = "AIzaSyDnZBEWmgvqLbnxvRDebxeyI8bz2CTmOjQ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,98 +34,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvResult = findViewById(R.id.tvResult);
-
-        // Initialize the SDK
-        Places.initialize(getApplicationContext(), "AIzaSyDnZBEWmgvqLbnxvRDebxeyI8bz2CTmOjQ");
-
-        // Create a new Places client instance
-        placesClient = Places.createClient(this);
-
-        // Call findCurrentPlace and handle the response (first check that the user has granted permission).
-        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            getlocation();
-        } else {
-            // A local method to request required permissions;
-            // See https://developer.android.com/training/permissions/requesting
-            getLocationPermission();
-        }
     }
 
-    private void getLocationPermission() {
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+    public void onClick(View view){
+        Intent locationPickerIntent = new LocationPickerActivity.Builder()
+                .withLocation(41.4036299, 2.1743558)
+                .withGeolocApiKey(API_KEY)
+                .shouldReturnOkOnBackPressed()
+//                .withStreetHidden()
+//                .withCityHidden()
+                .withZipCodeHidden()
+                .withSatelliteViewHidden()
+                .withGooglePlacesEnabled()
+                .withVoiceSearchHidden()
+                .build(this);
 
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_FINE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(this,
-                        new String[]{ACCESS_FINE_LOCATION},
-                        PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        startActivityForResult(locationPickerIntent, MAP_BUTTON_REQUEST_CODE);
+    }
 
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-                getlocation();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null){
+            double latitude = data.getDoubleExtra(LATITUDE, 0.0);
+            double longitude = data.getDoubleExtra(LONGITUDE, 0.0);
+            String address = data.getStringExtra(LOCATION_ADDRESS);
+            Address fullAddress = data.getParcelableExtra(ADDRESS);
+            String addressJson = "";
+            if (fullAddress != null) {
+                addressJson = new Gson().toJson(fullAddress);
             }
-        } else {
-            // Permission has already been granted
-            getlocation();
+
+            tvResult.setText("latitude:"+latitude+"|longitude:"+longitude+"|address:"+address+"|addressJson="+addressJson);
         }
-
     }
-
-    private void getlocation() {
-        // Use fields to define the data types to return.
-        List<Place.Field> placeFields = Collections.singletonList(Place.Field.NAME);
-
-        // Use the builder to create a FindCurrentPlaceRequest.
-        FindCurrentPlaceRequest request =
-                FindCurrentPlaceRequest.newInstance(placeFields);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this,
-                    new String[]{ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-            return;
-        }
-        Task<FindCurrentPlaceResponse> placeResponse = placesClient.findCurrentPlace(request);
-        placeResponse.addOnCompleteListener(task -> {
-            if (task.isSuccessful()){
-                FindCurrentPlaceResponse response = task.getResult();
-                for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
-                    String placeInfo = String.format("Place '%s' has likelihood: %f. \n address:%s.\n latitude:%f \n longitude:%f",
-                            placeLikelihood.getPlace().getName(),
-                            placeLikelihood.getLikelihood(),
-                            placeLikelihood.getPlace().getAddress(),
-                            placeLikelihood.getPlace().getLatLng().latitude,
-                            placeLikelihood.getPlace().getLatLng().longitude
-                            );
-                    Log.i(TAG, placeInfo);
-
-                    tvResult.setText(placeInfo);
-                }
-            } else {
-                Exception exception = task.getException();
-                if (exception instanceof ApiException) {
-                    ApiException apiException = (ApiException) exception;
-                    Log.e(TAG, "Place not found: " + apiException.getStatusCode());
-                }
-            }
-        });
-    }
-
-
 }
